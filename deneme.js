98ef697215b4017def11bb1d9d457a13e3d14129
hilal görgülüarslan <101900441+gorguluarslan@users.noreply.github.com>
2023-04-03 22:19:28 +0300

    dateproject

---
+++ deneme.js
@@ -0,0 +1,34 @@
+$(function(){
+    var card = $(".card");
+
+    card.on('mousemove', function (e) {
+        var x = e.clientX + $(window).scrollLeft() - $(this).offset().left;
+        var y = e.clientY + $(window).scrollTop()- $(this).offset().top;
+
+        var rY = map(x, 0, $(this).width(), -17, 17);
+        var rX = map(y, 0, $(this).height(), -17, 17);
+
+        $(this).children(".image").css("transform", "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)");
+    });
+
+    card.on('mouseenter', function () {
+        $(this).children(".image").css({
+                transition: "all " + 0.05 + "s" + " linear",
+                WebkitTransition: "all " + 0.05 + "s" + " linear"
+        });
+    });
+
+    card.on('mouseleave', function () {
+        $(this).children(".image").css({
+            transition: "all " + 0.2 + "s" + " linear",
+            WebkitTransition: "all " + 0.2 + "s" + " linear"
+        });
+
+        $(this).children(".image").css("transform", "rotateY(" + 0 + "deg)" + " " + "rotateX(" + 0 + "deg)");
+    });
+
+    function map(x, in_min, in_max, out_min, out_max)
+    {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+});
